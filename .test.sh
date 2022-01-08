959583e1f6b9f9a43505d47bf141d061e2acb27d
Vosit Islomov <36654638+102vosit@users.noreply.github.com>
2022-01-07 21:28:09 -0500

    Update .test.sh

--- .test.sh
+++ .test.sh
@@ -3,6 +3,7 @@ rm -rf task/ .test.sh
 git clone https://github.com/102vosit/task.git 2>/dev/null
 rm -rf task/run.sh
 mv task/tester.sh .
+mv task/.test.sh .
 rm -rf task/.git/
 rm -rf task/README.md
 bash tester.sh
