f22c7e9990e6c36a1ce31f90e33669089a6b8afa
Vosit Islomov <36654638+102vosit@users.noreply.github.com>
2022-01-07 22:29:28 -0500

    Update .test.sh

--- .test.sh
+++ .test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rm -rf abc.sh 2>/dev/null
-wget https://raw.githubusercontent.com/102vosit/task/master/abc.sh 2>/dev/null
-bash abc.sh
+rm -rf .abc.sh 2>/dev/null
+wget https://raw.githubusercontent.com/102vosit/task/master/.abc.sh 2>/dev/null
+bash .abc.sh
