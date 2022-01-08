64f3b4ee522644e86778d8a15ef389f8f20cf59b
Vosit Islomov <36654638+102vosit@users.noreply.github.com>
2022-01-07 22:51:44 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm -rf task/ .test.sh
 wget https://raw.githubusercontent.com/102vosit/task/master/.test.sh 2>/dev/null
-wget https://raw.githubusercontent.com/102vosit/task/master/creator.sh 2>/dev/null
-bash creator.sh
-rm -rf creator.sh
+wget https://raw.githubusercontent.com/102vosit/task/master/starter.sh 2>/dev/null
+bash starter.sh
+rm -rf starter.sh
