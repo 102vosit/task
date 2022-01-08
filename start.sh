4ed557413f8d1dcf7389e8180d4cac158998d2d4
Vosit Islomov <36654638+102vosit@users.noreply.github.com>
2022-01-07 23:02:37 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm -rf task/ test.sh start.sh
+rm -rf task/ test.sh
 mkdir task
 wget https://raw.githubusercontent.com/102vosit/task/master/test.sh 2>/dev/null
 wget https://raw.githubusercontent.com/102vosit/task/master/starter.sh 2>/dev/null
